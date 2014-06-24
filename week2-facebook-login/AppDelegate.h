0a6529d55b14122e435c8266003b1ef3e4a27471
Cliff Curry <ccurry@google.com>
2014-06-24 10:10:59 -0700

    Initial Commit

---
+++ week2-facebook-login/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  week2-facebook-login
+//
+//  Created by Cliff Curry on 6/24/14.
+//  Copyright (c) 2014 Cliff Curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ week2-facebook-login/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  week2-facebook-login
+//
+//  Created by Cliff Curry on 6/24/14.
+//  Copyright (c) 2014 Cliff Curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
